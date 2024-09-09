2729b23e12326a4d3aa6ba4c589809cfa50470a9
Kimmy Haecho <118002501+siwolee@users.noreply.github.com>
2024-09-09 16:02:22 +0900

    Save TypeArea.tsx.

---
+++ front/src/components/TypeArea.tsx
@@ -0,0 +1,16 @@
+const TypeArea = ({content}):{content: string}) => {
+    return (
+        <div classname="w100 h80 mx-flex d-relative">
+            <div id="blind-area">
+                
+            </div>
+            <div id="input-area">
+                <input type='text' />
+            </div>
+            
+        </div>
+        
+        )
+}
+
+export default TypeArea;
