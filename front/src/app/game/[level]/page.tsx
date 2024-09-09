079b51f4941a59b4eff8488959aa80680be8f9db
Kimmy Haecho <118002501+siwolee@users.noreply.github.com>
2024-09-09 16:02:28 +0900

    Save page.tsx.

--- front/src/app/game/[level]/page.tsx
+++ front/src/app/game/[level]/page.tsx
@@ -1,9 +1,22 @@
+interface GameDataProps {
+    title: string;
+    content: string;
+    language: string;
+}
+
 // 게임 실행 페이지
 // 게임 레벨별로 백엔드에서 받아온 데이터를 이용하여 코딩 진행
 const Game = ({ params }: { params: { level: string } }) => {
+  const gameData: GameDataProps = {
+      title: "write from 0 to 9",
+      language: c,
+      content: 'allowed functions: write'
+  } 
   return (
-    <div>
+    <div class={"game-container"}>
       <h1>Game level : {params.level}</h1>
+      <h2>Title : {gameData.title}<h2>
+      <
     </div>
   );
 };
